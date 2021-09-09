e98bb1bd84c04da3f96364bc8f0f70be5386f906
Thomas Lovén <thomasloven@gmail.com>
2021-09-09 15:10:35 +0200

    Cleanup

--- config.sh
+++ config.sh
@@ -1,16 +1,48 @@
-
+# Dry run. Must be explicitly set to 0 to perform actual file system operations.
 DRYRUN=0
-BACKUP_ROOT=/data/backup/test
-VERBOSITY=4
 
-KEEP_DAILY=7
-KEEP_WEEKLY=5
-KEEP_MONTHLY=12
+# Root directory of backup snapshot storage
+BACKUP_ROOT=/data/backup/snapper
 
+# Verbosity level determines how much ouput is written to stderr
+# 1: Critical errors
+# 2: Errors
+# 3: Warnings (default)
+# 4: Information
+# 5: Debug
+
+# VERBOSITY=4
+
+# All performed operations are logged to LOGFILE
+LOGFILE=./snapper.log
+
+# Numbers of each type of backup to retain
+# KEEP_DAILY=7
+# KEEP_WEEKLY=4
+# KEEP_MONTHLY=120
+
+# Additional arguments to RSYNC
+# RSYNC_ARGUMENTS=""
 RSYNC_ARGUMENTS="--exclude 'node_modules'"
 
+# Sources to backup from
+# A list of <protocol> <source> <target>
+# <protocol>:
+#   snapshot - makes a snapshot of a btrfs subvolume
+#   local - rsync from local source
+#   ssh - rsync over ssh
+# <source>: Source subvolume or rsync path
+#   note that rsync splits the path at a /./ and only retains what comes after it
+# <target>: Location in backup
+#   snapshots will be stored to BACKUP_ROOT/daily.0/<target>
+#   local and ssh will be stored to BACKUP_ROOT/daily.0/local/<target>
+#
+# Ex:
+# SOURCES="
+# snapshot /data/photos photos
+# local /home/./thomasloven server/homedir
+# ssh thomas@laptop:/home/./thomas laptop/homedir
+# "
+
 SOURCES="
-snapshot /data/backup/test/source snapped-source
-local /data/backup/test/source2 synced-source
-ssh caladan:~/code/./ arrakis-code
 "
